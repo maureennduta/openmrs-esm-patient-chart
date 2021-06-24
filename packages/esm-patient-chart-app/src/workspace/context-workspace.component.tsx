870f4667d966fef0509c2e2b0d14d921f373f088
Donald Kibet <chelashawdonald@yahoo.com>
2021-06-24 14:53:13 +0300

    Add ability to maximize and minimize workspace

--- packages/esm-patient-chart-app/src/workspace/context-workspace.component.tsx
+++ packages/esm-patient-chart-app/src/workspace/context-workspace.component.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import Close32 from '@carbon/icons-react/es/close/32';
+import ArrowRight16 from '@carbon/icons-react/es/arrow--right/16';
+import Maximize16 from '@carbon/icons-react/es/maximize/16';
+import Minimize16 from '@carbon/icons-react/es/minimize/16';
 import styles from './context-workspace.scss';
-import { ExtensionSlot, useLayoutType } from '@openmrs/esm-framework';
+import { ExtensionSlot, LayoutType, useLayoutType } from '@openmrs/esm-framework';
 import { RouteComponentProps } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useBodyScrollLock } from '@openmrs/esm-patient-common-lib';
@@ -9,10 +11,29 @@ import { Header, HeaderGlobalAction, HeaderGlobalBar, HeaderName } from 'carbon-
 import { useWorkspace } from '../hooks/useWorkspace';
 import { patientChartWorkspaceSlot } from '../constants';
 import { isDesktop } from '../utils';
-
 interface ContextWorkspaceParams {
   patientUuid: string;
 }
+
+type WindowState = 'maximize' | 'normal' | 'collapsed';
+type ActionTypes = 'minimize' | 'maximize' | 'collapse';
+
+interface WindowStatus {
+  maximize: boolean;
+  normal: boolean;
+  collapsed: boolean;
+}
+
+const reducer = (state: WindowState, action: ActionTypes) => {
+  switch (action) {
+    case 'maximize':
+      return (state = 'maximize');
+    case 'minimize':
+      return (state = 'normal');
+    case 'collapse':
+      return (state = 'collapsed');
+  }
+};
 
 const ContextWorkspace: React.FC<RouteComponentProps<ContextWorkspaceParams>> = ({ match }) => {
   const layout = useLayoutType();
@@ -20,20 +41,41 @@ const ContextWorkspace: React.FC<RouteComponentProps<ContextWorkspaceParams>> = 
   const { active, title, closeWorkspace } = useWorkspace();
   const { t } = useTranslation();
   const isTablet = layout === 'tablet';
+  const [windowState, updateWindowState] = React.useReducer(reducer, 'normal');
   const props = React.useMemo(
     () => ({ closeWorkspace, patientUuid, isTablet }),
     [closeWorkspace, isTablet, patientUuid],
   );
 
+  const Icon = windowState === 'maximize' ? Minimize16 : Maximize16;
+
+  const windowStateStyles = () => {
+    if (windowState === 'maximize') return styles.maximized;
+    if (windowState === 'collapsed') return styles.hide;
+  };
+
   useBodyScrollLock(active && !isDesktop(layout));
 
   return (
-    <aside className={styles.contextWorkspaceContainer} style={{ display: active ? 'inline' : 'none' }}>
+    <aside
+      className={`${styles.contextWorkspaceContainer} ${windowStateStyles()} ${active ? styles.show : styles.hide}`}
+      style={{ display: windowState === 'collapsed' && !active && 'none' }}>
       <Header aria-label={title} style={{ position: 'sticky' }}>
         <HeaderName prefix="">{title}</HeaderName>
         <HeaderGlobalBar>
-          <HeaderGlobalAction aria-label={t('close', 'Close')} title={t('close', 'Close')} onClick={closeWorkspace}>
-            <Close32 />
+          <HeaderGlobalAction
+            onClick={() => {
+              windowState === 'maximize' ? updateWindowState('minimize') : updateWindowState('maximize');
+            }}
+            aria-label={t('maximize', 'Maximize')}
+            title={t('maximize', 'Maximize')}>
+            <Icon />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction
+            aria-label={t('collapse', 'Collapse')}
+            title={t('collapse', 'Collapse')}
+            onClick={() => updateWindowState('collapse')}>
+            <ArrowRight16 />
           </HeaderGlobalAction>
         </HeaderGlobalBar>
       </Header>
